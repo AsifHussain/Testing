f78e701b015aa68c554855365f64397271e56eae
Asif <asif@Asifs-Mac-Pro.local>
2015-08-04 10:34:25 +0500

    Initial Commit

---
+++ NewTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewTestApp
+//
+//  Created by Asif on 04/08/2015.
+//  Copyright (c) 2015 Asif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTestApp
+//
+//  Created by Asif on 04/08/2015.
+//  Copyright (c) 2015 Asif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
